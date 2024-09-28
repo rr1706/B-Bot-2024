9286df8e749a4e1484a72be10fdec811be45ba3e
FRC1706 <driverstationfrc1706@outlook.com>
2024-09-28 10:47:58 -0500

    small changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -51,7 +51,7 @@ public class RobotContainer {
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.leftTrigger().onTrue(new InstantCommand(()->m_shooter.run(73, 20))).onFalse(new InstantCommand(()->m_shooter.stop()));
+    m_driverController.leftTrigger().onTrue(new InstantCommand(()->m_shooter.run(60, 25))).onFalse(new InstantCommand(()->m_shooter.stop()));
   }
 
   /**
@@ -60,7 +60,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
+    // An example command will be run in autonomous[\]
     return Autos.exampleAuto(m_exampleSubsystem);
   }
 }
